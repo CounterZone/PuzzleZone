8e6fafdbda147c257c6366bbefd00c73ae097f64
CounterZone <czczczczczczsss@gmail.com>
2020-09-01 15:49:38 -0500

    dev

--- PuzzleZone/settings.py
+++ PuzzleZone/settings.py
@@ -25,7 +25,7 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 SECRET_KEY = '+q+hqq&5)@hv4w+pg*p0xplxq2fkq(f#qtp-ki*)oojfkx*ivd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost']
 
