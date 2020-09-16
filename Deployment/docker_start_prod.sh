eba6cc5deb418457c35c5843f97cf820768743aa
CounterZone <czczczczczczsss@gmail.com>
2020-09-16 12:42:04 -0500

    fix

--- Deployment/docker_start_prod.sh
+++ Deployment/docker_start_prod.sh
@@ -3,8 +3,6 @@
 
 echo "SECRET_KEY='${PUZZLE_KEY}'" > ../PuzzleZone/secret_key.py
 
-docker volume create puzzlezone_user_test
-
 docker-compose -f docker-compose-prod.yml build
 
 docker build ../puzzle/Docker_test -t test_image
