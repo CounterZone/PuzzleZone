56be0bb2e6144e4e1c0afdda20a2bfbc3bbd952f
CounterZone <czczczczczczsss@gmail.com>
2021-03-02 06:30:44 -0600

    1

--- Deployment/clean-up.sh
+++ Deployment/clean-up.sh
@@ -1,3 +1,3 @@
-docker container stop $(docker ps -a -q --filter="name=puzzlezone_user_test_*")
+docker container stop $(docker ps -a -q --filter="name=puzzlezone_*")
 docker container rm $(docker ps -a -q --filter="name=puzzlezone_user_test_*")
 docker volume rm $(docker volume ls -q --filter="name=puzzlezone_user_test_*")
